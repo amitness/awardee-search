5d09070ce862fd07b199600bddbe0a7101c96445
Amit Chaudhary <studenton.com@gmail.com>
2017-02-27 16:45:42 +0545

    Run server only when directly run

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def search():
     else:
         return "Company not found."
 
-app.run('0.0.0.0', port=8000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
